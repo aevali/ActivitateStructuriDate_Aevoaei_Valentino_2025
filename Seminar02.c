ee26cd78fd8278c8f31ee730cd4aa63ae06bf6e6
Aevoaei Valentino <valiaevoaei07@gmail.com>
2025-03-07 18:28:20 +0200

    adaugarea functiei initializare

--- Seminar02.c
+++ Seminar02.c
@@ -9,6 +9,22 @@ struct Masina {
 	char normaPoluare;
 };
 
+struct Masina initializare(int id, int nrLocuri, const char* marca, float capacitateC, char normaPoluare) {
+	struct Masina m;
+	m.id = id;
+	m.nrLocuri = nrLocuri;
+	m.capacitateC = capacitateC;
+	m.normaPoluare = normaPoluare;
+	if (marca == NULL)
+		m.marca = NULL;
+	else
+	{
+		m.marca = (char*)malloc(strlen(marca) + 1);
+		strcpy_s(m.marca, strlen(marca) + 1, marca);
+	}
+	return m;
+}
+
 int main()
 {
 
