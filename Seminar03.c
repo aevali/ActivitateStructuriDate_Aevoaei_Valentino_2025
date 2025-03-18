4ad43b23d20b1efe5511a962a7f481f04ff713f0
Aevoaei Valentino <valiaevoaei07@gmail.com>
2025-03-19 01:56:22 +0200

    creare structura masina

---
+++ Seminar03.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct StructuraMasina {
+	int id;
+	int nrUsi;
+	float pret;
+	char* model;
+	char* numeSofer;
+	unsigned char serie;
+};
+typedef struct StructuraMasina Masina;
+
+int main()
+{
+
+}
