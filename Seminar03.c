de52a85e62f52533e4c830a1d3df2f428001bb94
Aevoaei Valentino <valiaevoaei07@gmail.com>
2025-03-19 02:10:38 +0200

    adaugare functie citire masina din fisier

--- Seminar03.c
+++ Seminar03.c
@@ -32,6 +32,26 @@ void afisareVectorMasini(Masina** masini, int* nrMasini, Masina masinaNoua) {
 	(*masini) = NULL;
 }
 
+Masina citireMasinaFisier(FILE* file) {
+	char buffer[100];
+	char sep[3] = ",\n";
+	fgets(buffer, 100, file);
+	char* aux;
+	Masina m1;
+	aux = strtok(buffer, sep);
+	m1.id = atoi(aux);
+	m1.nrUsi = atoi(strtok(NULL, sep));
+	m1.pret = atof(strtok(NULL, sep));
+	aux = strtok(NULL, sep);
+	m1.model = malloc(strlen(aux) + 1);
+	strcpy_s(m1.model, strlen(aux) + 1, aux);
+	aux = strtok(NULL, sep);
+	m1.numeSofer = malloc(strlen(aux) + 1);
+	strpy_s(m1.numeSofer, strlen(aux) + 1, aux);
+	m1.serie = *strtok(NULL, sep); // sau cu index de 0 [0]
+	return m1;
+}
+
 int main()
 {
 
